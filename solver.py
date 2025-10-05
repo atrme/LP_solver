ffdf9cd6f10306c48240fae53df8c72723608ca8
atrme <ischaoyangy@gmail.com>
2025-10-05 11:44:29 +0800

    Fix bug

--- solver.py
+++ solver.py
@@ -2,63 +2,85 @@ import scipy
 import sys
 
 import scipy.optimize
+from functools import reduce
 
+variables = []
+is_minimize = bool()
 
-def get_data(data):
+def parse(data):
+    def parse_expression(expr: str) -> dict:
+        ''' Parse out the coefficients and variables of an expression.
+            e.g. '2*x1-3*x2-x3+x4' -> {'x1': 2, 'x2': -3, 'x3': -1, 'x4': 1}
+        '''
+        expr = expr.replace('-', '+-')
+        partitions = expr.split('+')
+        result = dict()
+        for partition in partitions:
+            if '*' in partition: # Conditions like 2*x, -2*y
+                coefficient, variable = partition.split('*')
+                coefficient = float(coefficient)
+            else:
+                if '-' in partition: # Conditions like -x, -y
+                    variable = partition.replace('-', '')
+                    coefficient = -1
+                else: # Conditions like x, y
+                    variable = partition
+                    coefficient = 1
+            result[variable] = coefficient
+        return result
+
+    global variables, is_minimize
     data = data.split('\n') 
     data_obj = data[0]
     data_cons = data[1:]
     
-    # Get all variables
-    obj_neg = (-1 if data_obj.strip().partition("=")[0] == 'max' else 1)
-    data_obj_processed = data_obj.strip().partition("=")[2].split("+")
-    variables = [item.partition("*")[2] if ("*" in item) else item for item in data_obj_processed]
-    
-    # Get coefficients of variables in objective function
-    obj_coefficients = [float(item.partition("*")[0])*obj_neg if ("*" in item) else obj_neg for item in data_obj_processed]
+    # Get coefficients and variables in object expression
+    direction, obj_expr = data_obj.strip().split('=')
+    is_minimize = (direction == 'min')
+    obj_negative = (1 if is_minimize else -1)
+    obj_dict = parse_expression(obj_expr)
+
+    variables = list(obj_dict)
+    obj_coefficients = list( map(lambda x: obj_negative*x, obj_dict.values()) )
     
     # Get coefficients of variables and bounds in constraints
-    cons_coefficients = list()
-    cons_bounds = list()
+    all_constraint_coefficients = list()
+    all_constraint_bounds = list()
     for data_con in data_cons:
         if ">=" in data_con:
             deli = ">="
         elif "<=" in data_con:
             deli = "<="
+        elif ">" in data_con:
+            deli = ">"
+        elif "<" in data_con:
+            deli = "<"
         else:
             raise "Illegal constraint format"
         
-        data_con_processed = data_con.strip().partition(deli)
+        constraint_expr, bound = data_con.strip().split(deli)
         
         # Bound 
-        bound = float(data_con_processed[2])
+        bound = float(bound)
         
         # Coefficients
         coefficients = list()
-        data_con_processed2 = data_con_processed[0].split("+")
-        temp_dict = {item.split("*")[-1]: float(item.split("*")[0]) if "*" in item 
-                     else 1 
-                     for item in data_con_processed2}
-        for var in variables:
-            coefficient = 0
-            if var not in temp_dict:
-                coefficient = 0
-            else:
-                coefficient = temp_dict[var]
-            
-            coefficients.append(coefficient)
-        
-        # Multiply -1 when it is ">="
-        if deli == ">=":
+        constraint_dict = parse_expression(constraint_expr)
+        coefficients = [
+            constraint_dict[var] if var in constraint_dict else 0
+            for var in variables
+        ]
+
+        # Multiply -1 when it is ">=" or ">"
+        if deli in (">=", ">"):
             bound *= -1
-            for i in range(len(coefficients)):
-                coefficients[i] *= -1
+            coefficients = [-x for x in coefficients]
         
         
-        cons_bounds.append(bound)
-        cons_coefficients.append(coefficients)
+        all_constraint_bounds.append(bound)
+        all_constraint_coefficients.append(coefficients)
     
-    return (obj_coefficients, cons_coefficients, cons_bounds)
+    return (obj_coefficients, all_constraint_coefficients, all_constraint_bounds)
 
 
 def solve(obj_ce, constraint_ce, constraint_b):
@@ -76,12 +98,14 @@ def show(result):
             count += x
             if i != 0:
                 print("; ", end="")
-            print(f"x{i}={round(x, 2)}", end="")
-        print(f", min z={round(result.fun, 2) * (-1 if my_str[:3]=='max' else 1)}")
+            print(f"{variables[i]}={round(x, 2)}", end="")
+        print(f", {'min' if is_minimize else 'max'} z={round(result.fun, 2) * (1 if is_minimize else -1)}")
+    else:
+        print('There\'s no solution to this problem')
 
 def main():
     global my_str
-    obj_ce, cons_ce, cons_b = get_data(my_str)
+    obj_ce, cons_ce, cons_b = parse(my_str)
     result = solve(obj_ce, cons_ce, cons_b)
     show(result)
     
